8be0f24cb5c7e1e34fcf2d44d5e9157a414e16af
Khliupnev Vladimir <proksik49032@mail.ru>
2026-01-07 18:24:22 +0400

    feat: add terser

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,17 @@ export default defineConfig({
       }
     },
     sourcemap: true,
-    minify: true
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+        passes: 2
+      },
+      mangle: true,
+      format: {
+        comments: false
+      }
+    }
   }
 });
